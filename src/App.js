4d7bb95fb93bc1a75c7f5ccbb710abb589347b26
tylerpalmerdev <mr.tylerpalmer@gmail.com>
2022-10-27 11:01:14 -0700

    round trip fetch complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const BASE_API_ENDPOINT = 'http://ip-api.com/json/';
+
+  const [ipAddress, setIpAddress] = useState('');
+  const [responseData, setResponseData] = useState(null);
+  const [responseFields, setResponseFields] = useState(null);
+  const [fetchError, setFetchError] = useState(null);
+  const [selectedField, setSelectedField] = useState('');
+
+  const preventDefault = event => event.preventDefault();
+  
+  const onChangeIpAddress = event => {
+    preventDefault(event);
+
+    const { target: { value }} = event;
+    setIpAddress(value);
+  };
+
+  const onChangeSelectedField = event => {
+    preventDefault(event);
+
+    const { target: { value }} = event;
+    setSelectedField(value);
+  };
+
+  async function fetchIpData() {
+    try {
+      const endpoint = `${BASE_API_ENDPOINT}${ipAddress}`;
+      const response = await fetch(endpoint);
+      const data = await response.json();
+
+      setResponseData(data);
+      setResponseFields(Object.keys(data));
+    } catch (err) {
+      setFetchError(err);
+    }
+  }
+
   return (
     <main className="main-content">
       <h1>CTA IP Data Fetch Tool</h1>
-      <form>
+      <form onSubmit={preventDefault}>
         <label htmlFor="ipAddressInput">Enter an IP address:</label>
-        <input id="ipAddressInput" placeholder="0.0.0.0" />
-        <button>Fetch Data</button>
+        <input onChange={onChangeIpAddress} value={ipAddress} type="text" id="ipAddressInput" placeholder="0.0.0.0" />
+        <button onClick={fetchIpData}>Fetch Data</button>
       </form>
+      {responseFields && 
+        <div>
+          <label htmlFor="fieldSelector">Select a field to display:</label>
+          <select id="fieldSelector" value={selectedField} onChange={onChangeSelectedField}>
+            <option value="" disabled hidden>Select a field</option>
+            {responseFields.map(responseField =>
+              <option key={responseField} value={responseField}>{responseField}</option>
+            )}
+          </select>
+        </div>
+      }
+      {(selectedField && responseData) &&
+        <section data-testid="display-area">
+          <h3>Your data for {selectedField}:</h3>
+          <span>{responseData[selectedField]}</span>
+        </section>
+      }
     </main>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
+afterEach(() => {
+  // cleanup any fetch mocks
+  // @todo: cleanup
+  if (typeof fetch.mockClear === 'function') {
+    fetch.mockClear();
+  }
+});
+
 it('renders the default state as expected', () => {
-  expect.assertions(5);
+  expect.assertions(6);
 
   // when rendering the app
   render(<App />);
 
   // then the heading is visible
-  expect(screen.getByRole('heading', { name: 'CTA IP Data Fetch Tool'})).toBeInTheDocument();
+  expect(screen.getByRole('heading', { name: 'CTA IP Data Fetch Tool' })).toBeInTheDocument();
 
   // and the IP input element is visible
   const ipInput = screen.getByLabelText('Enter an IP address:');
   expect(ipInput).toBeInTheDocument();
+  expect(ipInput).toHaveAttribute('type', 'text');
 
   // and the submit button is visible
   expect(screen.getByRole('button', { name: 'Fetch Data' })).toBeInTheDocument();
@@ -24,7 +34,56 @@ it('renders the default state as expected', () => {
   expect(screen.queryByTestId('display-area')).not.toBeInTheDocument();
 });
 
-it.todo('allows user to enter IP address, fetch data, and select field as expected');
+it('allows user to enter IP address, fetch data, and select field as expected', async() => {
+  expect.assertions(9);
+
+  // mock & spy on the global fetch object
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ field1: 'value1', field2: 'value2', field3: 'value3' })
+    })
+  );
+
+  const testIp = '123.45.67.89';
+
+  // when rendering the app
+  render(<App />);
+
+  // and the user enters an IP address
+  const ipInput = screen.getByLabelText('Enter an IP address:');
+  await fireEvent.change(ipInput, { target: { value: testIp }});
+  // then the input's value updates
+  expect(ipInput).toHaveValue(testIp);
+
+  // when the user clicks 'Fetch Data'
+  await fireEvent.click(screen.getByRole('button', { name: 'Fetch Data' }));
+
+  await waitFor(() => {
+    // then a new dropdown is rendered
+    expect(screen.getByRole('combobox')).toBeInTheDocument();
+  });
+
+  // and the keys from the response object are rendered as options in a dropdown
+  expect(screen.getAllByRole('option')).toHaveLength(3);
+  
+  // and fetch has been called correctly
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch).toHaveBeenCalledWith(`http://ip-api.com/json/${testIp}`);
+
+  // when user selects an option from the dropdown
+  await userEvent.selectOptions(screen.getByRole('combobox'), screen.getByRole('option', { name: 'field1' }));
+
+  // that field is rendered in the display area
+  expect(screen.getByRole('heading', { name: 'Your data for field1:' })).toBeInTheDocument();
+  expect(screen.getByText(/value1/)).toBeInTheDocument();
+
+  // when user selects another option from the dropdown
+  await userEvent.selectOptions(screen.getByRole('combobox'), screen.getByRole('option', { name: 'field2' }));
+
+  // that new field is rendered in the display area
+  expect(screen.getByRole('heading', { name: 'Your data for field2:' })).toBeInTheDocument();
+  expect(screen.getByText(/value2/)).toBeInTheDocument();
+});
 
 it.todo('handles a server error as expected');
 
